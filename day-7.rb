e7e24b0a2ce56807cce4c766e7a4f0420c67840e
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2021-04-02 19:51:37 +0200

    Day 8: Part one

--- day-7.rb
+++ day-7.rb
@@ -12,7 +12,7 @@ main_bags = Hash.new {|h,k| h[k] = [] }
 data.each do |line|
   main_bag, contents = line.match(/(\w+ \w+) bags contain (.*)\./)[1, 2]
 
-  sub_bags = contents.split(",").map {|bag| bag.match(/(\w+ \w+) bag/)[1]}.compact
+  sub_bags = contents.split(",").map {|bag| bag.match(/(\w+ \w+) bag/)[1]}
 
   sub_bags.each do |sub_bag|
     main_bags[sub_bag] << main_bag
---
+++ day-8.rb
@@ -0,0 +1,33 @@
+# Part one
+
+require "set"
+
+data = []
+File.readlines("data_8.txt").each do |line|
+  data << line.strip
+end
+
+lines_executed = Set.new
+jump = 0 
+accumulator = 0
+
+while true
+  if lines_executed.include?(jump)
+    puts "Part one answer: #{ accumulator }"
+    exit
+  end
+
+  lines_executed << jump
+
+  operation, argument = data[jump].split
+  argument = argument.to_i
+
+  if operation == "acc"
+    accumulator = accumulator + argument
+  elsif operation == "jmp"
+    jump = jump + argument -1
+  end
+  
+  jump = jump + 1
+end
+
