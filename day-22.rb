d565b0f9514c4e7d9b06c24f03c46612493dde37
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2021-04-12 18:11:49 +0200

    Day 22: Part one

---
+++ day-22.rb
@@ -0,0 +1,36 @@
+data = []
+File.read("data_22.txt").split("\n\n").each do |player|
+  data << player
+end
+
+players_cards = data.map { |player| player.scan(/\n\d+/).map(&:to_i) }
+
+player_1_deck = players_cards[0]
+player_2_deck = players_cards[1]
+
+until player_1_deck.size == 0 || player_2_deck.size == 0
+    player_1_card = player_1_deck.shift
+    player_2_card = player_2_deck.shift
+
+    if player_1_card > player_2_card
+      player_1_deck << player_1_card
+      player_1_deck << player_2_card
+    else
+      player_2_deck << player_2_card
+      player_2_deck << player_1_card
+    end
+end
+
+score = 0
+
+if player_1_deck.size > player_2_deck.size
+  player_2_deck.reverse.each_with_index do |card, index|
+    score += card * (index + 1)
+  end
+else
+  player_2_deck.reverse.each_with_index do |card, index|
+    score += card * (index + 1)
+  end
+end
+
+puts "Part one answer: #{ score }"
