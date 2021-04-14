f0b45a03fc8c664cb6bfb212190adbe3456a2b5d
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2021-04-14 16:44:58 +0200

    Day 25: Part one

---
+++ day-25.rb
@@ -0,0 +1,34 @@
+# Part one
+
+data = []
+
+File.readlines("data_25.txt").each do |line|
+  data << line.strip.to_i
+end
+
+card_key = data[0]
+door_key = data[1]  
+  
+def loop_size (public_key)
+  loop_size = 0
+  key = 1
+  until key == public_key
+    key = (key * 7) % 20201227
+    loop_size += 1
+  end
+  loop_size
+end
+
+card_key_loop_size = loop_size(card_key)
+door_key_loop_size = loop_size(door_key)
+
+def encryption_key (public_key, loop_size)
+  key = 1
+  loop_size.times do
+    key = (key * public_key) % 20201227
+  end
+  key
+  end
+
+  key = encryption_key(card_key, door_key_loop_size)
+  puts "Part one answer: #{ key }" 
