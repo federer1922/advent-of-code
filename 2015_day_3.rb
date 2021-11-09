8dbaff5d61ccb8e1e456068dce40804e27823773
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2021-11-09 19:38:06 +0100

    2015 day 3

---
+++ 2015_day_3.rb
@@ -0,0 +1,72 @@
+require 'set'
+
+# Part one
+
+directions = File.read('2015_day_3.txt').chars
+
+houses_coordinates = Set.new
+x = 0
+y = 0
+houses_coordinates << [x, y]
+
+directions.each do |direction|
+  if direction == '^'
+    y += 1
+  elsif direction == 'v'
+    y -= 1
+  elsif direction == '>'
+    x += 1
+  else
+    x -= 1
+  end
+  houses_coordinates << [x, y]
+end
+
+puts "Part one answer: #{ houses_coordinates.size }"
+
+# Part two
+
+houses_coordinates = Set.new
+x = 0
+y = 0
+houses_coordinates << [x, y]
+santa_directions = []
+robot_directions = []
+robot_x = 0
+robot_y = 0
+
+directions.each_with_index do |direction, index|
+  if index.even?
+    santa_directions << direction
+  else
+    robot_directions << direction
+  end
+end
+
+santa_directions.each do |direction|
+  if direction == '^'
+    y += 1
+  elsif direction == 'v'
+    y -= 1
+  elsif direction == '>'
+    x += 1
+  else
+    x -= 1
+  end
+  houses_coordinates << [x, y]
+end
+
+robot_directions.each do |direction|
+  if direction == '^'
+    robot_y += 1
+  elsif direction == 'v'
+    robot_y -= 1
+  elsif direction == '>'
+    robot_x += 1
+  else
+    robot_x -= 1
+  end
+  houses_coordinates << [robot_x, robot_y]
+end
+  
+puts "Part two answer: #{ houses_coordinates.size }"
