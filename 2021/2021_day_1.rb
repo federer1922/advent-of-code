39702cc262feeaced8f79a4b2e59efef2d03082c
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2021-12-01 19:31:38 +0100

    day 1 2021

---
+++ 2021/2021_day_1.rb
@@ -0,0 +1,30 @@
+# Part one
+
+measurements = File.readlines('2021_day_1.txt').map(&:to_i)
+increased = 0
+previous_measurement = measurements[0]
+
+measurements.each do |measurement|
+  if measurement > previous_measurement
+    increased += 1
+  end
+  previous_measurement = measurement
+end
+
+puts "Part one answer: #{ increased }"
+
+# Part two
+
+measuring_trios = []
+measurements.each_cons(3) { |trio| measuring_trios << trio.sum }
+increased = 0
+previous_measurement = measuring_trios[0]
+
+measuring_trios.each do |measurement|
+  if measurement > previous_measurement
+    increased += 1
+  end
+  previous_measurement = measurement
+end
+
+puts "Part two answer: #{ increased }"
