35300dc877e79cc79840847d6cf7821cd2e626c6
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2022-01-14 15:27:57 +0100

    day 12 update, reducing computing time

--- 2021/day_12.rb
+++ 2021/day_12.rb
@@ -1,42 +1,48 @@
+# frozen_string_literal: true
+
+# Passage Pathing
 class Day12
   def initialize(file)
     lines = File.readlines(file).map(&:strip)
     @connected = Hash.new { |h, k| h[k] = [] }
 
     lines.each do |line|
-      from, to = line.split("-")
-      if from == "start" || to == "end"
+      from, to = line.split('-')
+      if from == 'start' || to == 'end'
         @connected[from] << to
-      elsif to == "start" || from == "end"
+      elsif to == 'start' || from == 'end'
         @connected[to] << from
       else
         @connected[from] << to
         @connected[to] << from
       end
     end
-    @connected
   end
 
-  def solve(part_1 = true)
-    to_visit = [["start"]]
+  def solve(part)
+    to_visit = [['start']]
     paths = []
 
     until to_visit.empty?
       path = to_visit.shift
 
       @connected[path.last].each do |cave|
-        if part_1
+        case part
+        when 1
           next if cave.downcase == cave && path.include?(cave)
-        else # Part two
-          small_caves = path.select { _1.downcase == _1 }
-          next if small_caves.size - small_caves.uniq.size > 1
-        end
 
-        if cave == "end"
-          paths << path + ["end"]
-        end
+          paths << path + ['end'] if cave == 'end'
+          to_visit << path + [cave]
+        when 2
+          next if cave.downcase == cave && path.include?(cave) && path.include?('twice')
 
-        to_visit << path + [cave]
+          paths << path + ['end'] if cave == 'end'
+          to_visit << if cave.downcase == cave && path.include?(cave)
+                        path + ['twice', cave]
+                      else
+                        path + [cave]
+                      end
+        end
       end
     end
     paths.size
@@ -45,13 +51,12 @@ end
 
 # Part one
 
-part_1 = Day12.new('day_12.txt').solve
+part_1 = Day12.new('day_12.txt').solve(1)
 
-puts "Part one answer: #{ part_1 }"
-
+puts "Part one answer: #{part_1}"
 
 # Part two
 
-part_2 = Day12.new('day_12.txt').solve(false)
+part_2 = Day12.new('day_12.txt').solve(2)
 
-puts "Part two answer: #{ part_2 }"
+puts "Part two answer: #{part_2}"
